a155e816071e2a519f4d86099593cf378237f6d3
viralpraxis <iaroslav2k@gmail.com>
2024-09-23 10:29:19 +0300

    Define shared context only for old rubocops

--- spec/shared_contexts.rb
+++ spec/shared_contexts.rb
@@ -1,29 +1,31 @@
 # frozen_string_literal: true
 
-RSpec.shared_context 'ruby 2.7' do
-  # Prism supports parsing Ruby 3.3+.
-  let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 2.7 }
-end
+unless RuboCop::Version.version >= '1.6'
+  RSpec.shared_context 'ruby 2.7' do
+    # Prism supports parsing Ruby 3.3+.
+    let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 2.7 }
+  end
 
-RSpec.shared_context 'ruby 3.0' do
-  # Prism supports parsing Ruby 3.3+.
-  let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 3.0 }
-end
+  RSpec.shared_context 'ruby 3.0' do
+    # Prism supports parsing Ruby 3.3+.
+    let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 3.0 }
+  end
 
-RSpec.shared_context 'ruby 3.1' do
-  # Prism supports parsing Ruby 3.3+.
-  let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 3.1 }
-end
+  RSpec.shared_context 'ruby 3.1' do
+    # Prism supports parsing Ruby 3.3+.
+    let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 3.1 }
+  end
 
-RSpec.shared_context 'ruby 3.2' do
-  # Prism supports parsing Ruby 3.3+.
-  let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 3.2 }
-end
+  RSpec.shared_context 'ruby 3.2' do
+    # Prism supports parsing Ruby 3.3+.
+    let(:ruby_version) { ENV['PARSER_ENGINE'] == 'parser_prism' ? 3.3 : 3.2 }
+  end
 
-RSpec.shared_context 'ruby 3.3' do
-  let(:ruby_version) { 3.3 }
-end
+  RSpec.shared_context 'ruby 3.3' do
+    let(:ruby_version) { 3.3 }
+  end
 
-RSpec.shared_context 'ruby 3.4' do
-  let(:ruby_version) { 3.4 }
+  RSpec.shared_context 'ruby 3.4' do
+    let(:ruby_version) { 3.4 }
+  end
 end
